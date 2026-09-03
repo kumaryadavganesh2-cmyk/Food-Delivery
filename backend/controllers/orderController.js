6c01e88ac165dac89423aa8754e4e7a0994ca56f
kumaryadavganesh2-cmyk <kumaryadavganesh2@gmail.com>
2026-09-03 12:42:18 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //config variables
 const currency = "inr";
 const deliveryCharge = 50;
-const frontend_url = "http://localhost:5174";
+const frontend_url = "https://food-delivery-frontend-q03u.onrender.com";
 
 // placing user order for frontend
 const placeOrder = async (req,res) => {
